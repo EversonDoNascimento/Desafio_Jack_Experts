97ab322484837a34c56c9bb8416c3877a86a32a3
EversonDoNascimento <everson25nascimento@gmail.com>
2024-08-30 20:09:10 -0300

    fix: Removing directory dist

--- Back-end/dist/utils/requestIntercepter.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.requestIntercepter = void 0;
-const requestIntercepter = (req, res, next) => {
-    console.log(`⇒ ${res.statusCode} ${req.method} ${req.originalUrl} ${JSON.stringify(req.body)}`);
-    next();
-};
-exports.requestIntercepter = requestIntercepter;
